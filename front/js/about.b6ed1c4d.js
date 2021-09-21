04ffd50bdde8c9256278d266a770b0a06cf86f13
Erica@Kali <erica@tresorier.me>
2021-09-21 22:28:55 +0200

    reorganize 2

---
+++ front/js/about.b6ed1c4d.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(n,t,e){"use strict";e.r(t);var a=e("7a23"),s={class:"about"},c=Object(a["h"])("h1",null,"This is an about page",-1);function o(n,t){return Object(a["s"])(),Object(a["e"])("div",s,[c])}const u={};u.render=o;t["default"]=u}}]);
+//# sourceMappingURL=about.b6ed1c4d.js.map
